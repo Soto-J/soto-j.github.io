bba9597c2c3a87a45fba12b74fcc18edd35d2a84
John Soto <soto.ja415@gmail.com>
2022-07-29 17:23:19 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-const navbar = document.querySelector(".nav-bar");
-window.addEventListener("scroll", () => {
-    navbar.classList.toggle("sticky", window.scrollY > 100);
+const btnNavToggle = document.querySelector(".nav-bar__navigation-toggle-btn");
+const navBarLinks = document.querySelector(".nav-bar__navigation-links");
+
+btnNavToggle.addEventListener("click", () => {
+    navBarLinks.classList.toggle("nav-bar__navigation-links-active");
 });
+// const nav = document.querySelector(".navigation");
+// const introSection = document.querySelector(".main__intro");
+// const sectionOneOptions = {};
+
+// const introObserver = new IntersectionObserver((entries, introObserver) => {
+//     entries.forEach((entry) => {
+//         if (!entry.isIntersecting) {
+//             nav.style.position = "fixed";
+//         }
+//     });
+// }, sectionOneOptions);
+
+// introObserver.observe(introSection);
